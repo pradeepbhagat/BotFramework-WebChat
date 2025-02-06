0034739e02e3458f9af524b5db63e8f4ac47125d
Rushikesh Gavali <rushikesh.gavali@nuance.com>
2025-02-06 19:03:51 +0530

    botframework bundle

---
+++ packages/test/botframework-webchat.js
@@ -0,0 +1,60 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var _chunk6WJ4IRYMjs = require('./chunk-6WJ4IRYM.js');
+
+
+
+
+
+
+
+
+
+
+
+
+var _chunkWU5TCC3Vjs = require('./chunk-WU5TCC3V.js');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.Components = _chunk6WJ4IRYMjs.Components; exports.Constants = _chunkWU5TCC3Vjs.Constants; exports.ReactSpeechWebChat = _chunk6WJ4IRYMjs.FullReactSpeechWebChat_default; exports.ReactWebChat = _chunk6WJ4IRYMjs.FullReactWebChat_default; exports.buildInfo = _chunk6WJ4IRYMjs.buildInfo; exports.concatMiddleware = _chunkWU5TCC3Vjs.concatMiddleware; exports.createAdaptiveCardsAttachmentForScreenReaderMiddleware = _chunk6WJ4IRYMjs.createAdaptiveCardsAttachmentMiddleware; exports.createAdaptiveCardsAttachmentMiddleware = _chunk6WJ4IRYMjs.createAdaptiveCardsAttachmentMiddleware2; exports.createBrowserWebSpeechPonyfillFactory = _chunkWU5TCC3Vjs.createBrowserWebSpeechPonyfillFactory; exports.createCognitiveServicesSpeechServicesPonyfillFactory = _chunk6WJ4IRYMjs.createCognitiveServicesSpeechServicesPonyfillFactory; exports.createDirectLine = _chunk6WJ4IRYMjs.createDirectLine; exports.createDirectLineAppServiceExtension = _chunk6WJ4IRYMjs.createDirectLineAppServiceExtension; exports.createDirectLineSpeechAdapters = _chunk6WJ4IRYMjs.createDirectLineSpeechAdapters; exports.createStore = _chunkWU5TCC3Vjs.createStore; exports.createStoreWithDevTools = _chunkWU5TCC3Vjs.createStoreWithDevTools; exports.createStoreWithOptions = _chunkWU5TCC3Vjs.createStoreWithOptions; exports.createStyleSet = _chunk6WJ4IRYMjs.createFullStyleSet; exports.decorator = _chunkWU5TCC3Vjs.decorator; exports.default = _chunk6WJ4IRYMjs.exports_default; exports.hooks = _chunk6WJ4IRYMjs.patchedHooks; exports.internal = _chunkWU5TCC3Vjs.internal; exports.renderMarkdown = _chunk6WJ4IRYMjs.render; exports.renderWebChat = _chunk6WJ4IRYMjs.renderWebChat; exports.testIds = _chunkWU5TCC3Vjs.testIds; exports.version = _chunkWU5TCC3Vjs.version; exports.withEmoji = _chunkWU5TCC3Vjs.withEmoji;
+/*! For license information please see botframework-webchat.js.LEGAL.txt */
+//# sourceMappingURL=botframework-webchat.js.map
